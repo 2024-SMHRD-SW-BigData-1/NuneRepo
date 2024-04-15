a550e39590673819b7d39c2d261e81e2ad675a21
NHW <worker1492@naver.com>
2024-04-15 09:44:08 +0900

    팀장 커밋

--- src/Ex01.java
+++ src/Ex01.java
@@ -6,13 +6,9 @@ public class Ex01 {
 
 
 		System.out.println("팀장 첫번째 커밋");
-<<<<<<< HEAD
-		System.out.println("팀원 두번째 커밋");
-		
-=======
-		System.out.println("팀원1 첫번째 커밋");
 
->>>>>>> ff7961eeeb084a629f6952068cb0cc03362f7600
+
+
 	}
 
 }
