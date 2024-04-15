63fd55d3848e2d2cdb96ad55b15337f7a5828d5f
NHW <worker1492@naver.com>
2024-04-15 11:39:51 +0900

    branch 추가

--- src/Ex01.java
+++ src/Ex01.java
@@ -2,13 +2,18 @@
 public class Ex01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 
 		System.out.println("팀장 첫번째 커밋");
 		System.out.println("팀원22 두번째 커밋");
 		System.out.println("팀원33 세번째 커밋");
 		System.out.println("미니프로젝트 파이팅");
+		
+		System.out.println("Test123");
+		
+
+		System.out.println("나만의 작업공간(branch)에 커밋하기!!");
+		
 	}
 
 }
